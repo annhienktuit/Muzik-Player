1c36492a7ddc69b602e184c3d3f446ed654b0b8a
Nhien Nguyen <53686305+annhienktuit@users.noreply.github.com>
2022-01-17 14:39:18 +0700

    feat: disallow fragment recreation

--- app/src/main/java/com/annhienktuit/muzikplayer/activities/HomeActivity.kt
+++ app/src/main/java/com/annhienktuit/muzikplayer/activities/HomeActivity.kt
@@ -2,12 +2,26 @@ package com.annhienktuit.muzikplayer.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.annhienktuit.muzikplayer.R
+import com.annhienktuit.muzikplayer.fragments.LocalMusicFragment
 import com.annhienktuit.muzikplayer.fragments.MusicFragment
+import com.annhienktuit.muzikplayer.fragments.OnlineMusicFragment
 import com.annhienktuit.muzikplayer.fragments.VideoFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+import androidx.annotation.NonNull
+
+
+
+
 class HomeActivity : AppCompatActivity() {
+    val fragmentMusic: Fragment = MusicFragment()
+    val fragmentVideo: Fragment = VideoFragment()
+    val fm: FragmentManager = supportFragmentManager
+    var activeFragment: Fragment = fragmentMusic
+
     private lateinit var bottomNav: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,37 +31,26 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun setupFragment() {
-        val fragment = when (bottomNav.selectedItemId) {
-            R.id.nav_music -> MusicFragment()
-            R.id.nav_video -> VideoFragment()
-            else -> {
-                MusicFragment()
-            }
-        }
+        fm.beginTransaction().add(R.id.fragment_container, fragmentVideo, "Video").hide(fragmentVideo).commit()
+        fm.beginTransaction().add(R.id.fragment_container,fragmentMusic, "Music").commit()
 
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragment_container, fragment)
-        transaction.commit()
-
-        val mOnBottomNavigationView = BottomNavigationView.OnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.nav_music -> {
-                    val transaction = supportFragmentManager.beginTransaction()
-                    transaction.replace(R.id.fragment_container, MusicFragment())
-                    transaction.commit()
-                    return@OnNavigationItemSelectedListener true
+        val mOnNavigationItemSelectedListener =
+            BottomNavigationView.OnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.nav_music -> {
+                        fm.beginTransaction().hide(activeFragment).show(fragmentMusic).commit()
+                        activeFragment = fragmentMusic
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.nav_video -> {
+                        fm.beginTransaction().hide(activeFragment).show(fragmentVideo).commit()
+                        activeFragment = fragmentVideo
+                        return@OnNavigationItemSelectedListener true
+                    }
                 }
-                R.id.nav_video -> {
-                    val transaction = supportFragmentManager.beginTransaction()
-                    transaction.replace(R.id.fragment_container, VideoFragment())
-                    transaction.commit()
-                    return@OnNavigationItemSelectedListener true
-                }
+                false
             }
-            false
-        }
-
-        bottomNav.setOnItemSelectedListener(mOnBottomNavigationView)
+        bottomNav.setOnItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
     private fun attachView() {
